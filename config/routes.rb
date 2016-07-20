c5168a1f96dcf3efbf7c43fc6ce0e09055e70e10
Kora Saccharin <kora.saccharin@gmail.com>
2016-07-20 16:40:09 +0200

    #users – generation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :visualizations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
